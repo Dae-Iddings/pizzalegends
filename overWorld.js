5c22c27c4546847b9166a7f62df16352e3826c67
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2023-01-03 11:16:00 -0600

    sizes hero correctly and gets just on sprite

--- overWorld.js
+++ overWorld.js
@@ -11,6 +11,24 @@ class Overworld {
         this.ctx.drawImage(image, 0, 0); // what image to draw and x y cord of where to draw starting in top left corner
         };
         image.src = 'images/maps/DemoLower.png';
+        // canvas images stack new things cover up old
+        const x = 0;
+        const y = 0;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero, // what we are drawing
+                0, //  left cut
+                0, // top cut
+                32, // width of cut
+                32, // height of cut
+                x,
+                y,
+                32, //size of character 
+                32, //size of character
+                )
+        }
+        hero.src = 'images/characters/people/hero.png'
     }
 
 }
